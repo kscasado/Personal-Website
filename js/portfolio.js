ebd882fc5a29cd83f307a697a686a83c4e438ad6
Kevin Casado <kcasado@Kevins-MacBook-Pro-2.local>
2016-07-21 18:24:23 -0700

    added improvements to resume.html

--- js/portfolio.js
+++ js/portfolio.js
@@ -9,6 +9,7 @@ portfolio.config(function($stateProvider, $urlRouterProvider){
             })
             .state('resume',{
               url:'/resume',
+              controller:'pdfCtrl',
               templateUrl: 'pages/resume.html'
             })
             .state('projects', {
@@ -29,3 +30,28 @@ portfolio.controller('cntrl' ,["$scope", "$location", function($scope,$location)
             $location.path(view);
         }
     }]);
+    portfolio.controller('pdfCtrl', function($scope) {
+
+      $scope.pdfName = 'Relativity: The Special and General Theory by Albert Einstein';
+      $scope.pdfUrl = 'res/resume.pdf';
+      $scope.scroll = 0;
+      $scope.loading = 'loading';
+
+      $scope.getNavStyle = function(scroll) {
+        if(scroll > 100) return 'pdf-controls fixed';
+        else return 'pdf-controls';
+      }
+
+      $scope.onError = function(error) {
+        console.log(error);
+      }
+
+      $scope.onLoad = function() {
+        $scope.loading = '';
+      }
+
+      $scope.onProgress = function(progress) {
+        console.log(progress);
+      }
+
+    });
